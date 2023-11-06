95ec77b58f8515a5738883d202ad9ca7dd50c00c
Shenya De Silva <shenya44@outlook.com>
2023-11-07 00:24:22 +0100

    Included libft.h

--- main.c
+++ main.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "MLX42/MLX42.h"
+#include "include/libft.h"
 #define WIDTH 256
 #define HEIGHT 256
 
 int32_t	main(void)
 {
 	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "Wireframe", true);
-
 	mlx_loop(mlx);
+	ft_printf("..FDF shutdown. See you tomorrow!\n");
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
 }
