96cca82944ee7549d79e0ed5e974fde842fa7460
Shenya De Silva <shenya44@outlook.com>
2023-11-07 14:14:40 +0100

    App parser and usage comments added

--- main.c
+++ main.c
@@ -6,11 +6,32 @@
 #define WIDTH 256
 #define HEIGHT 256
 
-int32_t	main(void)
+int32_t launch_mlx_window(void)
 {
 	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "Wireframe", true);
 	mlx_loop(mlx);
-	ft_printf("..FDF shutdown. See you tomorrow!\n");
+	ft_printf("..fdf shutdown. See you tomorrow!\n");
 	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
+}
+
+void ft_parse_map(char *str)
+{
+	ft_printf("... %s file exists.\n", str);
+	ft_printf("... %s is a valid map.\n", str);
+	ft_printf("... %s is parsed and saved.\n", str);
+}
+
+
+int32_t	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		ft_parse_map(argv[1]);
+		ft_printf("...Initializing %s\n", argv[1]);
+		launch_mlx_window();
+	}
+	else
+		ft_printf("Usage: ./fdf <mapfile name>\n");
 	return (EXIT_SUCCESS);
 }
