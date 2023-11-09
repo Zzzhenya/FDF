6b9bbb032e146bd748833351dc2bc9d0c155da03
Shenya De Silva <shenya44@outlook.com>
2023-11-09 01:30:18 +0100

    Basic error handling for open, close and fily type

--- main.c
+++ main.c
@@ -1,10 +1,24 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include "MLX42/MLX42.h"
-#include "include/libft.h"
-#define WIDTH 256
-#define HEIGHT 256
+#include "fdf.h"
+
+int	ft_strstr(const char *haystack, const char *needle)
+{
+	size_t		i;
+	size_t		j;	
+
+	i = 0;
+	j = 0;
+	while (haystack[i] != '\0' && needle[j] != '\0')
+	{
+		if (haystack[i] == needle[j])
+			j ++;
+		i ++;
+	}
+	if (j == ft_strlen(needle))
+		return (1);
+	else
+		return (0);
+
+}
 
 int32_t launch_mlx_window(void)
 {
@@ -17,6 +31,21 @@ int32_t launch_mlx_window(void)
 
 void ft_parse_map(char *str)
 {
+	int fd;
+
+	if (!ft_strstr(str, ".fdf"))
+	{
+		ft_printf("Incorrect file type. It should be .fdf\n");
+		exit(0);
+	}
+	fd = open (str, O_RDONLY);
+	if (fd < 0)
+	{
+		ft_printf("open() error. \n");
+		if (close(fd) < 0)
+			ft_printf("close() error. \n");
+		exit(0);
+	}
 	ft_printf("... %s file exists.\n", str);
 	ft_printf("... %s is a valid map.\n", str);
 	ft_printf("... %s is parsed and saved.\n", str);
