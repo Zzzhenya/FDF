698c7559ee2188b11d7dd7a58ca12ef61bee1ea4
Shenya De Silva <shenya44@outlook.com>
2023-11-09 19:49:39 +0100

    Restructured function according to norm

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "fdf.h"
 
-int	ft_strstr(const char *haystack, const char *needle)
+static int	ft_strstr(const char *haystack, const char *needle)
 {
 	size_t		i;
 	size_t		j;	
@@ -19,21 +19,29 @@ int	ft_strstr(const char *haystack, const char *needle)
 		return (0);
 }
 
-int32_t launch_mlx_window(void)
+int launch_mlx_window(void)
 {
 	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "Wireframe", true);
 	mlx_loop(mlx);
-	ft_printf("..fdf shutdown. See you tomorrow!\n");
+	ft_printf("... fdf shutdown. See you tomorrow!\n");
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
 }
 
-int	check_for_shape(int fd, t_obj map)
+void	free_arr(char **arr, int cols)
 {
-	char	*line;
-	char	**arr;
+	while (cols > -1)
+	{
+		free(arr[cols]);
+		cols --;
+	}
+}
+
+static int check_first_line (int fd)
+{
 	int 	cols;
-	int 	rows;
+	char	**arr;
+	char 	*line;
 
 	line = get_next_line(fd);
 	if (!line)
@@ -45,12 +53,18 @@ int	check_for_shape(int fd, t_obj map)
 	cols = 0;
 	while (arr[cols])
 		cols ++;
-	map.x_max = cols;
-	while (cols > -1)
-	{
-		free(arr[cols]);
-		cols --;
-	}
+	free_arr (arr, cols);
+	return (cols);
+}
+
+static int	check_for_shape(int fd, t_obj map)
+{
+	char	*line;
+	char	**arr;
+	int 	cols;
+	int 	rows;
+
+	map.x_max = check_first_line(fd);
 	rows = 0;
 	while (1)
 	{
@@ -63,22 +77,13 @@ int	check_for_shape(int fd, t_obj map)
 			cols ++;
 		if (cols < map.x_max || cols > map.x_max)
 		{
-			while (cols > -1)
-			{
-				free(arr[cols]);
-				cols --;
-			}
+			free_arr (arr, cols);
 			free(line);
+			close (fd);
 			return (-1);
 		}
 		else
-		{
-			while (cols > -1)
-			{
-				free(arr[cols]);
-				cols --;
-			}
-		}
+			free_arr (arr, cols);
 		free(line);
 		rows ++;
 	}
@@ -96,17 +101,23 @@ void ft_fdf(char *str)
 	map.y_max = 0;
 	map.x_max = 0;
 	if (!ft_strstr(str, ".fdf"))
-		ft_errexit("Incorrect file type. It should be .fdf");
+		ft_errexit("Incorrect file type. It should be a .fdf");
+	ft_printf("... Correct file type.\n", str);
+
 	fd = open (str, O_RDONLY);
 	if (fd < 0)
 		ft_errexit("open() error.");
 	ft_printf("... %s file exists.\n", str);
+
 	if (check_for_shape(fd, map) < 0)
 		ft_errexit("Map is not a rectangle.");
-	// map is not numeric
-	ft_printf("... %s is a valid map.\n", str);
-	if (close (fd) < 0)
-		ft_errexit("close() error.");
+	ft_printf("... %s is a rectangular map.\n", str);
+
+	/* check for numeric values, NULL, INT MAX and INT MIN
+	if (parse_and_store(str, map) < 0)
+		ft_errexit("Parse and store error");
+	ft_printf("... %s is a valid map.\n", str);*/
+
 	ft_printf("... %s is parsed and saved.\n", str);
 }
 
