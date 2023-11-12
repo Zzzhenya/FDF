583878513dd874b0b77e5790a7d73af6ace8b6f8
Shenya De Silva <shenya44@outlook.com>
2023-11-12 23:23:05 +0100

    Initial commit

---
+++ ft_calc_coords.c
@@ -0,0 +1,39 @@
+#include "fdf.h"
+/*
+void ft_init_iso(t_obj *map)
+{
+	
+	linked list of t_verts as void *content
+	size of (y_max * x_max) long
+	
+	t_vert vert;
+	vert = malloc(sizeof (t_vert));
+	vert.x = x_val;
+	vert.y = y_val;
+	(*map).iso = ft_ft_lstnew(void *(vert));
+}
+
+
+void calc_iso_coords(t_obj *map)
+{
+	int x;
+	int y;
+	int x_val;
+	int y_val;
+
+	x = 0;
+	while (x < (*map).y_max)
+	{
+		y = 0;
+		while (y < (*map).x_max)
+		{
+			x_val = x + cos(120) * (*map).coord[x][y];
+			y_val = y + sin(120) * (*map).coord[x][y];
+			ft_printf("x,y : (%d , %d)\n", x_val, y_val);
+			mlx_put_pixel(g_img, x_val, y_val, 0xFFFFFFFF);
+			y ++;
+		}
+		x ++;
+	}
+}
+*/
