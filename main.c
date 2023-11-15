81abb6b218ebcd1752503bfd2440bb9705cf499e
Shenya De Silva <shenya44@outlook.com>
2023-11-15 21:25:22 +0100

    Removed unused function

--- main.c
+++ main.c
@@ -12,33 +12,7 @@
 
 #include "fdf.h"
 #include <stdio.h>
-/*
-void draw_verts(t_obj *map, mlx_image_t *g_img)
-{
-	int x;
-	int y;
-	int x_val;
-	int y_val;
 
-	y = 0;
-	while (y < (*map).y_max)
-	{
-		x = 0;
-		while (x < (*map).x_max)
-		{
-			
-			y_val = (y + cos(120) * (*map).coord[y][x]) * 40;
-			x_val = (x + sin(120) * (*map).coord[y][x]) * 40;
-			
-			
-			ft_printf("x,y : (%d , %d)\n", x_val, y_val);
-			mlx_put_pixel(g_img, x_val, y_val, 0xFFFFFFFF);
-			x ++;
-		}
-		y ++;
-	}
-}
-*/
 void draw_verts(t_obj *map, mlx_image_t *g_img)
 {
 	int i;
@@ -60,9 +34,6 @@ int launch_mlx_window(t_obj	*map)
 	mlx = mlx_init(WIDTH, HEIGHT, "Wireframe", true);
 	g_img = mlx_new_image(mlx, WIDTH, HEIGHT);   // Creates a new image.
 	mlx_image_to_window(mlx, g_img, WIDTH* 2/5, HEIGHT * 2/5);  // Adds an image to the render queue.
-//    mlx_image_to_window(mlx, g_img, 0, 0);  // Adds an image to the render queue.
-    // Draw pixels
-    //mlx_put_pixel(g_img, 64, 64, 0xFFFFFFFF); // Single white pixel in the middle.
     draw_verts(map, g_img);
 	ft_printf("cols :%d\nrows :%d\n", (*map).x_max, (*map).y_max);
 	// Connect the dots here
